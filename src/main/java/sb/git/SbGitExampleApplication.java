ff3e84f2c58a1cba2416c6556da9a3ffe7d90609
kaustubhchoudhary <kaustubhchoudhary@yahoo.com>
2023-08-28 19:53:30 +0530

    Changed the greeting hello

--- src/main/java/sb/git/SbGitExampleApplication.java
+++ src/main/java/sb/git/SbGitExampleApplication.java
@@ -8,7 +8,7 @@ public class SbGitExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbGitExampleApplication.class, args);
-		System.out.println("Hi");
+		System.out.println("Hello");
 	}
 
 }

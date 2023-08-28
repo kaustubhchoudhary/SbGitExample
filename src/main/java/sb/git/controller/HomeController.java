f87f755440562dd4a9230a7fb67d7c1c1dfc85b0
kaustubhchoudhary <kaustubhchoudhary@yahoo.com>
2023-08-28 17:03:06 +0530

    1. Created a controller and a handler method to handle the initial request. 2. Set up the server port to 8081.

---
+++ src/main/java/sb/git/controller/HomeController.java
@@ -0,0 +1,13 @@
+package sb.git.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String method1() {
+		return "Welcome";
+	}
+}
